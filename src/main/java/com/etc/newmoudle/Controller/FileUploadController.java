7f509f1b656743e3a4f88f00f82aa091915e7067
zhanglang111 <1840624227@qq.com>
2019-12-26 21:57:39 +0800

    想实现任意用户的客户端上传与文件查找，发现对我来说有点困难，暂时打住

--- src/main/java/com/etc/newmoudle/Controller/FileUploadController.java
+++ src/main/java/com/etc/newmoudle/Controller/FileUploadController.java
@@ -1,8 +1,10 @@
 package com.etc.newmoudle.Controller;
 
 import java.io.File;
+import java.io.IOException;
 
 
+import ch.qos.logback.core.util.FileUtil;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,17 +14,37 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
+
+@RequestMapping("/upload")
 public class FileUploadController {
 
     @RequestMapping("douploadRemote")
-    public String douploadRemote(HttpServletRequest request, @RequestParam("file") MultipartFile multipartFile) throws Exception{
+    public String douploadRemote(HttpServletRequest request, @RequestParam("dir") MultipartFile[] dir) throws Exception{
 
-        if (multipartFile.isEmpty()) {
-            return "file is empty.";
-        }
-        String DocPath = ResourceUtils.getURL("classpath:").getPath();
 
-        System.out.println(DocPath);
+//        System.out.println(rootPath);
+
+//        System.out.println("上传文件夹...");
+//        File file;
+//        String fileName="";
+//        String filePath="";
+//        for (MultipartFile f : dir) {
+//            fileName=f.getOriginalFilename();
+//            String type=f.getContentType();
+//            System.out.println("\n"+fileName+" ,"+type);
+//            filePath="D:\\upload\\"+fileName.substring(0,fileName.lastIndexOf("/"));
+//
+//            System.out.println(filePath);
+//
+//            file = new File("D:\\upload\\" + fileName);
+//            try {
+//                file.createNewFile();
+//                //将上传文件保存到一个目标文件中
+//                f.transferTo(file);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        }
         return "FileUploadSuccess";
     }
 }
